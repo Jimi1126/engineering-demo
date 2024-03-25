1c73301aa2757da4266f7d492bf6b4b84594340e
Jimi1126 <xfqing_mid@163.com>
2024-03-25 18:56:51 +0800

    feat：前端工程化demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 :class="$style.blue">{{ msg }}</h1>
+  </div>
+</template>
+
+<style module>
+blue {
+  color: blue;
+}
+</style>
